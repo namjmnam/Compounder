c90b93404a4660fbbf313fc61e57abb884028f85
namjmnam <namjmnam@naver.com>
2021-02-24 16:53:59 +0900

    Update simplersplitter.py
    Attempt to fix regex error with strings containing parenthesis

--- simplersplitter.py
+++ simplersplitter.py
@@ -3,6 +3,7 @@ import math
 import re
 
 import sys
+from typing import Counter
 platform = sys.platform
 if platform.startswith('win32'):
     from eunjeon import Mecab # type: ignore
@@ -42,9 +43,11 @@ class Splitter:
         else: self.corpus = self.clean_str(' ' + inputCorpus)
 
         # wTotal = 말뭉치 총 어절 수
-        # self.wTotal = self.corpus.count(' ')
         # 현재 말뭉치 미사용
-        self.wTotal = self.rawdoc.count(' ')
+        # 문제점: 띄어쓰기의 갯수를 검색하고 있음
+        # self.wTotal = self.corpus.count(' ')
+        # self.wTotal = self.rawdoc.count(' ')
+        self.wTotal = len(re.findall(r"""[가-힣]+[ .,`'"‘’()]""", self.rawdoc)) + len(re.findall(r"""[가-힣]$""", self.rawdoc)) # 문제해결, 재확인 필요
 
         l = self.doc.split(' ')
         self.eoList = [i for i in l if len(re.sub(r'[0-9]+', '-', i)) >= 3]
@@ -53,13 +56,26 @@ class Splitter:
         missed = []
         for i in self.eoList:
             if i.count("(") > 0 and i.count(")") > 0:
+                # l = i[i.find("(")+1:i.find(")")]
+                # if l != '': missed.append(l)
+                # l = i[i.find(")")+1:i.find("(")]
+                # if l != '': missed.append(l)
                 missed.append(i[i.find("(")+1:i.find(")")])
+                missed.append(i[i.find(")")+1:i.find("(")])
                 continue
             if i.count("(") > 0:
-                missed.append(i.split("(",1)[1])
+                # missed.append(i.split("(",1)[1])
+                l = i.split("(")
+                missed += l
             if i.count(")") > 0:
-                missed.append(i[:-1])
-        parenthesisless = [x for x in self.eoList if  not '(' in x and not ')' in x] + [x for x in self.eoList if '(' in x and ')' in x]
+                # missed.append(i[:-1])
+                l = i.split(")")
+                missed += l
+        # 짧은 명사 제거
+        temp = missed[:]
+        for i in temp:
+            if len(i) < 2: missed.remove(i)
+        parenthesisless = [x for x in self.eoList if  '(' not in x and ')' not in x] + [x for x in self.eoList if '(' in x and ')' in x]
         parenthesisless += missed
         self.eoList = parenthesisless # 괄호가 한 쪽만 포함된 어절을 모두 제거하고 괄호 속 어절을 포함
 
@@ -77,12 +93,27 @@ class Splitter:
             finalscore = 0
             chosen = ''
             for j in range(len(i)):
-                # 현재는 단순히 대상에 띄어쓰기+단어가 검색된 갯수만 찾지만 본래 어절의 좌측부분만 검색하도록 해야 함
                 # 문제점1: 말뭉치는 클렌징이 되어있지 않음
                 # 문제점2: 기존에 이미 발견된 명사를 제외한 말뭉치에서 검색해야 함
                 # 문제점3: 말뭉치에 대하여 검색하고 있지 않음
+
+                # 문서 내 단어 수 산출
+                # 문제점: 등장횟수를 띄어쓰기 + 단어에 대해서 검색하고 있음
+                # 단순히 대상에 띄어쓰기+단어가 검색된 갯수만 찾음
                 # scores.append(self.corpus.count(' ' + i[j]) / self.wTotal)
-                scores.append(self.doc.count(' ' + i[j]) / self.wTotal)
+                # scores.append(self.doc.count(' ' + i[j]) / self.wTotal)
+
+                # 문제점: 너무 조잡함
+                str = re.sub('[^-/.\w]', repl='', string=i[j])
+                # 더 포괄적인 패턴
+                # patternone = r"""[ .,!?`'"‘’()]|[^\w]{}""".format(str)
+                patternone = r"""[ .,!?`'"‘’()]{}""".format(str)
+                patterntwo = r"^{}".format(str)
+                # 단어 앞 공백/기호가 오는 갯수 + 맨 처음에 오는 갯수
+                wordcount = len(re.findall(patternone, self.rawdoc) + re.findall(patterntwo, self.rawdoc))
+                # scores.append(wordcount / self.wTotal)
+                # 빈도율 계산 안 하고 빈도수만 계산
+                scores.append(wordcount)
             for j in range(len(scores)):
                 if j >= len(scores)-1:
                     chosen = i[j]
@@ -96,10 +127,26 @@ class Splitter:
                     break
                 finalscore = scores[j]
             # 빈도율이 2/어절수 이상인 경우 채택
-            if finalscore >= 2 / self.wTotal: self.extnouns.append(chosen)
+            # if finalscore >= 2 / self.wTotal: self.extnouns.append(chosen)
+            # 빈도율 계산 안 하고 빈도수만 계산 (2번 이상 등장)
+            if finalscore >= 2: self.extnouns.append(chosen)
         self.extnouns = list(dict.fromkeys(self.extnouns))
 
-        # 여기서 Mecab은 기존에 등록된 명사인지 아닌지 판단하기 위해 + 끝에 조사가 오는 단어를 제거하기 위해 사용
+        # 괄호가 한 쪽만 포함된 문자열 제거: regex가 에러를 냄
+        temp = self.extnouns[:]
+        for i in temp:
+            if i.count("(") > 0 and i.count(")") == 0:
+                self.extnouns.remove(i)
+                continue
+            if i.count(")") > 0 and i.count("(") == 0:
+                self.extnouns.remove(i)
+                continue
+            # 예: "이)런경(우" 제외
+            if i.count("(") > 0 and i.count(")") > 0 and i.index(")") > i.index("("):
+                self.extnouns.remove(i)
+                continue
+
+        # 여기서 Mecab은 끝에 조사가 오는 단어를 제거하기 위해 사용
         m = Mecab()
         # m = Mecab(dicpath='C:/mecab/mecab-ko-dic') # (사용불가능, 비활성)
         # m = Mecab(dicpath='/usr/local/lib/mecab/dic/mecab-ko-dic') # (사용불가능, 비활성)
@@ -209,7 +256,8 @@ class Splitter:
             trpmiListOld.append(trpmi)
         self.trpmidictold = dict(zip(cwListOld, trpmiListOld))
 
-        # Mecab을 사용하여 기존에 등록되어있는 명사 제외 (가장 마지막에 실행되어야 함)
+        # 여기서 Mecab은 기존에 등록된 명사인지 아닌지 판단하기 위해 사용 
+        # 기존에 등록되어있는 명사 제외 (가장 마지막에 실행되어야 함)
         temp = self.extnouns[:]
         for i in temp:
             if len(m.pos(i)) == 1 and m.pos(i)[0][1][0] == 'N':
@@ -305,9 +353,11 @@ class Splitter:
     # 한 어절에 대해서 모든 2자 이상의 LP 나열 리스트
     def genLP(self, eojeol):
         out = []
+        # 추출된 어절 끝에 마침표가 오는 경우 제거
         if eojeol[-1] == '.': eojeol = eojeol[:-1]
         for i in range(2, len(eojeol)+1):
-            out.append(eojeol[:i])
+            if len(eojeol[:i].replace('.', '')) > 1 and eojeol[:1][-1] != '.':
+                out.append(eojeol[:i])
         return out
 
     # 리스트 형태의 복합단어를 문서 전체에서 검색
@@ -332,7 +382,11 @@ class Splitter:
     # 단어에 연결된 명사를 리스트로 출력 (unweighted, directed)
     def wordMapping(self, word, nounlist, doc, ndist):
         out = []
-        sentList = doc.split('. ')
+        # 도태된 방식
+        # sentList = doc.split('. ')
+        # 새로운 방식
+        sentList = re.findall('.*?다\.*', doc)
+        sentList.append(re.sub('.*?다\.', '', doc))
         # 문장당 해당 단어 주위에 등장하는 문자열 구축
         surrounding = ''
         for i in sentList:
@@ -354,7 +408,11 @@ class Splitter:
     # 단어에 연결된 명사를 리스트로 출력 (compounder 방식, weighted, undirected)
     def wordMappingOld(self, word, nounlist, doc):
         out = []
-        sentList = doc.split('. ')
+        # 도태된 방식
+        # sentList = doc.split('. ')
+        # 새로운 방식
+        sentList = re.findall('.*?다\.*', doc)
+        sentList.append(re.sub('.*?다\.', '', doc))
         # 같은 문장 안에 등장하는 갯수 만큼 등록
         for i in sentList:
             temp = i
@@ -420,7 +478,11 @@ class Splitter:
     def extractFList(self, nounlist, doc):
         out = []
         templist = []
-        sentList = doc.split('. ')
+        # 도태된 방식
+        # sentList = doc.split('. ')
+        # 새로운 방식
+        sentList = re.findall('.*?다\.*', doc)
+        sentList.append(re.sub('.*?다\.', '', doc))
         for i in sentList:
             temp = i
             sublist = []
@@ -452,6 +514,7 @@ text = r"11월 입찰 예정 서울시 구로구 구로동에 위치한 `센터�
 # text = r"글로벌빅데이터연구소,?약?22만개?사이트?대상?9개?증권사?빅데이터?분석투자자?관심도?1위는?하나금융투자,?관심도?상승률?1위는?미래에셋대우  [파이낸셜뉴스]지난해 국내 주요 증권사에 대한 투자자 관심도를 조사한 결과 '하나금융투자'가 가장 높았던 것으로 나타났다. 같은 기간 관심도 상승률은 '미래에셋대우'가 가장 높았다.   5일 글로벌빅데이터연구소는 지난해 온라인 22만개 사이트를 대상으로 국내 9개 증권사에 대해 빅데이터를 분석한 결과, 이 같은 결과가 도출됐다고 밝혔다. 정보량의 경우 2019년과의 비교 분석도 실시했다.   연구소가 임의선정한 분석 대상 증권사는 '정보량 순'으로 △하나금융투자 △미래에셋대우 △NH투자증권 △키움증권 △삼성증권 △신한금융투자 △한국투자증권 △KB증권 △대신증권(대표 오익근) 등 이다.   분석 결과 온라인 게시물 수(총정보량)를 의미하는 '투자자 관심도'의 경우 2020년 '하나금융투자'는 총 30만2318건을 기록, 2019년 21만8533건에 비해 8만3785건 38.34% 늘어나며 1위를 차지했다.   이들 자료를 일일이 클릭한 결과 하나금융투자 정보량 중 '리포트'가 높은 비중을 차지했으며 투자자들은 이들 리포트를 블로그나 커뮤니티 등에 다시 게시하는 경우가 많았다.   정보량 2위는 지난해 총 29만1151건을 기록한 '미래에셋대우'였다. '미래에셋대우'는 지난 2019년 17만4672건에 비해서 11만6479건 66.68% 대폭 급증하며 증가량은 물론 증가율면에서 9개 주요 증권사중 가장 높았다.   2019년 26만3473건으로 가장 높은 관심도를 기록했던 'NH투자증권'은 지난해 2만3795건 9.03% 늘어 28만7268건을 보이는데 그치며 3위를 차지했다.   이어 '키움증권', '삼성증권', '신한금융투자', '한국투자증권', 'KB증권' 등이 20만~26만건 대를 기록하며 큰 차이를 보이지 않았으나 2019년 대비 증가량은 5.97%부터 50.72%까지 천차만별이었다.   관심도가 가장 낮은 '대신증권'은 지난해 총 19만7532건으로 2019년 13만8974건에 비해서는 5만8558건 42.14% 늘었다.   9개 증권사 중 가장 높은 투자자 호감도를 기록한 곳은 '하나금융투자'로 나타났다. 리포트 주목도가 높았던 '하나금융투자'는 긍정률에서 부정률을 뺀 값인 '순호감도'에서 41.94%를 기록, 1위를 차지했다.   정보량 상승률 1위였던 '미래에셋대우'가 28.94%로 순호감도에서 2위를 차지하며 '하나금융투자'와 함께 두 부문 모두 높은 지표를 보였다.   이어 '삼성증권' 25.78%, '한국투자증권' 25.36%, 'NH투자증권' 23.84%, '신한금융투자' 22.97%, '키움증권' 22.50%, 'KB증권' 21.41% 순이었다.   '대신증권'은 15.35%로 순호감도 역시 가장 낮았다"
 # text = r"우리나라의 1인가구가 매년 증가세를 보이는 가운데 지난해에는 600만을 넘어섰다. 가구 분포 역시 1인 가구는 30.2%를 차지해 2인 가구(27.8%), 3인 가구(20.7%), 4인 이상(21.2%)를 크게 앞지르며 대세를 이루고 있다. 특히 대전지역의 1인 가구 비율은 33.7%로 전년(32.6%) 대비 1.1% 증가했고 전국 1인 가구 비율보다는 3.5%가 더 높은 것으로 조사됐다. 향후 5년간 1인 가구 수가 매년 15만가구씩 증가할 것이라는 예측이 나오면서 유통업계에서는 1인 가구를 위한 소포장, 1인 메뉴 등을 출시하는데 열을 올리고 있다. 이러한 트렌드에 발맞춰 대전지역 롯데마트 3개 지점에서는 ‘한끼밥상’이라는 테마로 소포장 전문 코너를 만들어 운영 중이다. 농림축산식품부의 GAP(우수관리인증) 농산물을 990원부터 만나볼 수 있어 소비자로부터 좋은 반응을 보이고 있다. 실제로 대전지역 롯데마트 3개 지점(대덕점, 노은점, 서대전점)의 2020년 신선식품 中 소용량 상품군의 매출은 전년대비 12% 가까이 증가했다. 롯데마트 충청호남영업부문 배효권 부문장은 “1인 가구가 유통시장의 새로운 소비 주체로 떠오르면서 1~2인 가구를 겨냥한 소포장, 가정간편식 등과 같은 시장의 규모가 급성장할 것으로 예상된다”며 “롯데마트에서는 지역의 생산자와 손잡고 해당 상품군을 지속적으로 확대∙강화하는데 최선을 다하겠다”고 말했다."
 # text = r"정부가 이르면 26일께 3월부터 적용할 새 거리두기 조정안 단계를 발표할 예정이다. 오는 28일 현행 거리두기 단계(수도권 2단계, 비수도권 1.5단계) 종료 이후 사회적 거리두기가 재상향될지 관심이 모아진다.    손영래 중앙사고수습본부 사회전략반장은 23일 코로나19 백브리핑에서  (이번주) 금요일(26일) 또는 토요일(27일) 정도 생각 중인데 내일 정례브리핑 때 정확히 공지하겠다 고 밝혔다.    설 연휴 이후 600명대까지 치솟았던 일일 확진자수가 이틀 연속 300명대를 유지했지만 정부는 다시 증가할 가능성이 크다고 전망했다.    손 반장은  오늘까지는 주말 검사 감소량으로 인한 확진자 감소 현상이 나타났다고 본다 며  내일부터는 조금 증가할 것 같고, 26일까지 증가 추이가 어느 정도 갈지 봐야 한다 고 말했다.    앞서 정부는 지난 18일 다음 달부터 업종별 집합금지를 최소화하는 대신 개인 간 사적모임을 규제하는 자율과 책임에 기반을 골자로 하는 기본 방향을 내놨다.    이번 사회적 거리두기 개편안에는 현행 5단계(1→1.5→2→2.5→3단계)의 단점을 보완하는 대책도 담길 전망이다. 앞서 정부는 지난해 6월 3단계 체계의 거리두기를 적용하다가 같은해 11월 5단계로 개편한 바 있다. 0.5단계 차이로 세분화돼 있는 현행 체계는 단계별 대국민 행동 메시지가 분명하지 않아 위험성을 인지하기가 쉽지 않다는 지적이 제기돼 왔다.    식당이나 카페 등 다중이용시설에 대해서는 영업을 금지하는 집합금지는 최소화할 예정이다. 다만, 시설의 감염 취약 요인을 제거하기 위한 밀집도를 조정하기 위한 '인원제한'은 이어간다는 방침이다.    정세균 국무총리는 이날 중대본 회의에서  방역수칙 위반 업소에 대해서는 현재 시행 중인 '원스트라이크 아웃 제도'를 예외 없이 적용하고 곧 지급할 4차 재난지원금 지원 대상에서도 제외할 것 이라고 강조했다."
+# text = r"연초부터 무섭게 솟아오르던 비트코인 가격이 조정을 보이고 있다. 주요 투자 기관들의 잇따른 참여에도 불구, 미국 정부가 비트코인의 안정성과 적법성에 대해 강한 의구심을 표하면서 참여자들 사이에서 거품 논란과 규제 이슈 등으로 불안감이 형성된 탓이다. 그럼에도 이젠 비트코인 투자에 유의해야 할 때란 의견과 단기 조정을 거쳐 재반등할 것이란 주장이 팽팽히 맞서고 있다.    지난주 사상 첫 5만달러대에 진입한 비트코인 가격은 24일 현재 4만달러대로 떨어졌다. 재닛 옐런 미 재무장관이 지난 23일 뉴욕타임스 딜북 콘퍼런스에서 비트코인에 대해 “화폐를 거래하는 데 극도로(extremely) 비효율적인 방법”이라며 “투기성이 강한 자산이며, 극도로(extremely) 변동성이 있단 점을 인지해야 한다”고 말했다.    이처럼 옐런의 입에서 ‘극도로’란 표현을 여러번 사용할 정도로 비트코인에 대해 강한 경계 발언이 나온 것을 기점으로 시장의 우려가 증폭됐다. 안 그래도 일론 머스크 테슬라 최고경영자(CEO)가 가상자산 가격이 높아 보인다고 발언한 상황에서 기름을 끼얹는 격이었다.  마크 해펠 UBS 글로벌 자산운용 최고투자책임자(CIO)는 성명을 통해 “우리는 고객들에게 가상자산 투기에 주의를 기울여야 한다고 조언하고 있다”며 “규제 리스크가 아직 해소되지 않은 상황에서 (비트코인의) 미래는 여전이 불투명하다”고 밝혔다. 미국 투자 전문지 배런스도 비트코인의 버블이 터줄 수 있어 관련주 역풍에 주의해야 한다고 보도했다.    우리나라에서도 비트코인에 대한 우려 목소리가 커지고 있다. 이주열 한국은행 총재는 지난 23일 가상자산에 대해 ‘내재가치(intrinsic value)’가 없다고 평가했다. 내재가치는 자산가치와 수익가치를 아우른 개념으로 우리나라 중앙은행의 수장이 비트코인을 공인 자산으로 인정받기 어렵다는 견해를 밝힌 것이라고 볼 수 있다.  한편 비트코인 강세론자들은 현재의 하락 국면이 추가 매수 유인이 될 수 있다는 입장이다. 캐시 우드 아크 인베스트 CEO는 한 인터뷰에서 “우리는 비트코인에 대해 매우 긍정적이며, 지금 건강한 조정(healthy correction)을 볼 수 있어 매우 행복하다”고 말했다.    전세계 처음으로 캐나다에서 출시된 비트코인 상장지수펀드(퍼포즈 비트코인 ETF)는 흥행 기록을 이어가고 있다. 가상자산 분석업체 글라스노드에 따르면 퍼포즈 ETF로의 자금 유입이 지속되면서 23일 현재 운용규모(AUM)가 5억6400만달러(약 6300억원)에 달하고 있다.  "
 s = Splitter(text)
 print(s.extnouns)
 # print(s.conndict)
