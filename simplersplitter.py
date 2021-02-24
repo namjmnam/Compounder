bab49f19d4e06ca78b68dab968563259d6bfd000
namjmnam <namjmnam@naver.com>
2021-02-24 11:32:20 +0900

    Update simplersplitter.py
    Added support for two different ways of handling data

--- simplersplitter.py
+++ simplersplitter.py
@@ -17,8 +17,8 @@ class Splitter:
     def __init__(self, inputText, inputCorpus=None):
         
         # 원문 문서에서 신조어 추출
-        # 파이썬 버전 3.6
-        # 설치할 패키지: eunjeon, pandas
+        # 파이썬 버전 3.6, 3.8(윈도우 + eunjeon으로 실험)
+        # 설치할 패키지: eunjeon
         # 차후 eunjeon에서 konlpy로 이전 예정
 
         # 리눅스 환경 mecab-ko-dic 설치과정
@@ -141,45 +141,73 @@ class Splitter:
             # 문자열 앞뒤 ndist 개 내 검색 (TR 점수 합산이 1이 안 됨 -> 원인 불명) unweighted, directed
             # self.conndict[i] = self.wordMapping(i, self.extnouns, self.rawdoc, ndist)
 
-        # 복합단어 추출 프로토타입 (새 방식)
-        self.pairList = []
-        for i in self.extnouns:
-            for j in self.extnouns:
-                # rawdoc으로 검색할 수도 있고 corpus로 검색할 수도 있음
-                if self.searchSpaceless([i, j], self.rawdoc): self.pairList.append([i ,j])
-
-        cwlist = []
-        for i in self.pairList:
-            cwlist.append(' '.join(i))
-
-        pmiList = []
-        for i in self.pairList:
-            pmiList.append(self.getPMI(i, self.wTotal, self.rawdoc))
-
-        # PMI, TR 산출
-        self.pmidict = dict(zip(cwlist, pmiList))
+        # TR 산출 : 신규/기존 방식 혼용 가능
         self.trdict = self.calculateTextRank(self.extnouns, self.conndict, 16)
 
-        trpmiList = []
-        for i in self.pairList:
+        # 복합단어 추출 프로토타입 (신규방식) : 모든 복합단어를 for문으로 돌려서 동시출현하는 경우를 검색
+        self.pairListNew = []
+        for i in self.extnouns:
+            for j in self.extnouns:
+                # rawdoc으로 검색할 수도 있고 corpus로 검색할 수도 있음 (기존의 2번과 달리 1번 이상 등장하는 경우 등록)
+                if self.searchSpaceless([i, j], self.rawdoc) > 0 or self.searchSpaceless([i, j], self.parenthesisBuster(self.rawdoc)) > 0\
+                    and [i, j] not in self.pairListNew : self.pairListNew.append([i ,j])
+
+        # 신규방식으로 추출한 복합단어를 한 문자열로 묶은 리스트
+        cwListNew = []
+        for i in self.pairListNew:
+            cwListNew.append(' '.join(i))
+
+        # 신규방식으로 추출한 복합단어의 PMI 산출
+        pmiListNew = []
+        for i in self.pairListNew:
+            pmiListNew.append(self.getPMI(i, self.wTotal, self.rawdoc))
+        self.pmidictnew = dict(zip(cwListNew, pmiListNew))
+
+        # 신규방식으로 추출한 복합단어의 TR+PMI 산출
+        trpmiListNew = []
+        for i in self.pairListNew:
             trpmi = 1
             for j in i:
                 trpmi *= self.trdict[j]
             trpmi **= 1/len(i)
-            trpmi *= self.pmidict[' '.join(i)]
-            trpmiList.append(trpmi)
-        self.trpmidict = dict(zip(cwlist, trpmiList))
+            trpmi *= self.pmidictnew[' '.join(i)]
+            trpmiListNew.append(trpmi)
+        self.trpmidictnew = dict(zip(cwListNew, trpmiListNew))
 
         # 기존의 복합단어 추출 (compounder 방식)
         self.fList = self.extractFList(self.extnouns, self.rawdoc)        
-        self.allCW = []
+        self.pairListOld = []
         for i in range(len(self.fList)):
             n = self.genCW(self.fList[i])
             for j in n:
                 # 문서 내에 복합단어 후보가 존재하거나 괄호 내용을 제거한 문서 내에 복합단어 후보가 존재하고 이미 후보가 추가되지 않은 경우
-                if (self.searchSpaceless(j, self.rawdoc) > 1 or self.searchSpaceless(j, self.parenthesisButer(self.rawdoc)) > 1)\
-                    and j not in self.allCW:
-                    self.allCW.append(j)
+                if (self.searchSpaceless(j, self.rawdoc) > 1 or self.searchSpaceless(j, self.parenthesisBuster(self.rawdoc)) > 1)\
+                    and j not in self.pairListOld:
+                    self.pairListOld.append(j)
+        # 일부분 중복되는 복합단어를 탐지한 뒤 추가
+        self.pairListOld += self.detectRedundant(self.pairListOld)
+
+        # 기존방식으로 추출한 복합단어를 한 문자열로 묶은 리스트
+        cwListOld = []
+        for i in self.pairListOld:
+            cwListOld.append(' '.join(i))
+
+        # 기존방식으로 추출한 복합단어의 PMI 산출
+        pmiListOld = []
+        for i in self.pairListOld:
+            pmiListOld.append(self.getPMI(i, self.wTotal, self.rawdoc))
+        self.pmidictold = dict(zip(cwListOld, pmiListOld))
+
+        # 기존방식으로 추출한 복합단어의 TR+PMI 산출
+        trpmiListOld = []
+        for i in self.pairListOld:
+            trpmi = 1
+            for j in i:
+                trpmi *= self.trdict[j]
+            trpmi **= 1/len(i)
+            trpmi *= self.pmidictold[' '.join(i)]
+            trpmiListOld.append(trpmi)
+        self.trpmidictold = dict(zip(cwListOld, trpmiListOld))
 
         # Mecab을 사용하여 기존에 등록되어있는 명사 제외 (가장 마지막에 실행되어야 함)
         temp = self.extnouns[:]
@@ -187,7 +215,41 @@ class Splitter:
             if len(m.pos(i)) == 1 and m.pos(i)[0][1][0] == 'N':
                 self.extnouns.remove(i)
 
-    # compounder에서 사용하는 복합어일 가능성이 있는 wordpair 추출 방식
+    # 리스트의 두 번째부터 마지막 엘리먼트가 리스트의 첫 번째부터 마지막 두 번째 엘리먼트까지 같은 경우를 모두 찾아냄 (기존방식 전용)
+    def detectRedundant(self, cwlist):
+        out = []
+        indexchain = []
+        inchain = False
+        chainlist = []
+        # 리스트 길이-1번 반복
+        for i in range(len(cwlist)-1):
+            # 첫 번째를 제거한 i리스트와 마지막을 제거한 i+1리스트가 같은 경우
+            if cwlist[i][1:] == cwlist[i+1][:-1]:
+                # 체인이 끊겨있는 상태인 경우 (머리)
+                if inchain == False:
+                    inchain = True
+                    indexchain.append(i)
+                # 체인을 연결 (몸통)
+                else:
+                    indexchain.append(i)
+            # 체인이 끊기는 경우 (꼬리)
+            elif inchain == True:
+                inchain = False
+                indexchain.append(i)
+                chainlist.append(indexchain)
+                indexchain = []
+        # 출력 리스트 구축
+        added = []
+        for i in chainlist:
+            added = cwlist[i[0]][:]
+            if len(i) > 1:
+                for j in i:
+                    if j < i[-1]:
+                        added.append(cwlist[j+1][-1])
+            out.append(added)
+        return out
+
+    # compounder에서 사용하는 복합어일 가능성이 있는 wordpair 추출 방식 (기존방식 전용)
     def genCW(self, nounlist):
         out = []
         wordpair = []
@@ -297,9 +359,14 @@ class Splitter:
         for i in sentList:
             temp = i
             for j in nounlist:
-                while word in temp and j in temp:
-                    out.append(j)
-                    temp = temp.replace(j, '', 1)
+                # 새로운 방식
+                if word in temp and j in temp:
+                    for i in range(temp.count(j)): out.append(j)
+                # 도태된 방식
+                # while word in temp and j in temp:
+                #     # replace 방식이 아닌 append를 특정 수 만큼 해야
+                #     out.append(j)
+                #     temp = temp.replace(j, '', 1)
         out.remove(word)
         return out
 
@@ -329,7 +396,7 @@ class Splitter:
         node = dict(zip(nounlist, values))
         return node
 
-    # 단어페어 리스트에서 연결고리가 있을 가능성이 있는 경우를 모두 발견
+    # 단어페어 리스트에서 연결고리가 있을 가능성이 있는 경우를 모두 발견 (신규방식용)
     def detectConnection(self, cwlist):
         # 앞뒤 잘린 리스트 구축
         frontshortened = []
@@ -339,7 +406,7 @@ class Splitter:
         for i in cwlist:
             backshortened.append(i[:-1])
         
-        # 사전작업
+        # 사전구축작업
         connectionmap = {}
         for i in range(len(cwlist)):
             connectionmap[i] = []
@@ -349,7 +416,7 @@ class Splitter:
                 if frontshortened[i] == backshortened[j]: connectionmap[i].append(j)
         return connectionmap
 
-    # 명사 리스트와 원문에서 flist 추출
+    # 명사 리스트와 원문에서 flist 추출 (기존방식용)
     def extractFList(self, nounlist, doc):
         out = []
         templist = []
@@ -372,7 +439,7 @@ class Splitter:
         return out
 
     # 원문에서 괄호 속에 있는 문자열 제거
-    def parenthesisButer(self, doc):
+    def parenthesisBuster(self, doc):
         # 괄호를 찾아 사이에 있는 모든 문자열(괄호 속 괄호 제외) 모두 제거
         out = doc
         for i in range(doc.count('(')):
@@ -384,19 +451,23 @@ class Splitter:
 text = r"11월 입찰 예정 서울시 구로구 구로동에 위치한 `센터포인트 웨스트(구 서부금융센터)` 마스턴투자운용은 서울시 구로구 구로동 '센터포인트 웨스트(옛 서부금융센터)' 매각에 속도를 낸다.27일 관련업계에 따르면 마스턴투자운용은 지난달 삼정KPMG·폴스트먼앤코 아시아 컨소시엄을 매각 주관사로 선정한 후 현재 잠재 매수자에게 투자설명서(IM)를 배포하고 있는 단계다. 입찰은 11월 중순 예정이다.2007년 12월 준공된 '센터포인트 웨스트'는 지하 7층~지상 40층, 연면적 9만5000여㎡(약 2만8000평) 규모의 프라임급 오피스다. 판매동(테크노마트)과 사무동으로 이뤄졌다. 마스턴투자운용의 소유분은 사무동 지하 1층부터 지상 40층이다. 지하 1층과 지상 10층은 판매시설이고 나머지는 업무시설이다. 주요 임차인으로는 삼성카드, 우리카드, 삼성화재, 교보생명, 한화생명 등이 있다. 임차인의 대부분이 신용도가 높은 대기업 계열사 혹은 우량한 금융 및 보험사 등이다.'센터포인트 웨스트'는 서울 서남부 신도림 권역 내 최고층 빌딩으로 초광역 교통 연결성을 보유한 오피스 입지를 갖췄다고 평가받는다. 최근 신도림·영등포 권역은 타임스퀘어, 영시티, 디큐브시티 등 프라임급 오피스들과 함께 형성된 신흥 업무 권역으로 주목받고 있다고 회사 측은 설명했다.마스턴투자운용 측은   2021년 1분기를 클로징 예상 시점으로 잡고 있다  며   신도림 권역의 랜드마크로서 임대 수요가 꾸준해 안정적인 배당이 가능한 투자상품이 될 것  이라고 설명했다.한편 마스턴투자운용은 지난 2017년 말 신한BNP파리바자산운용으로부터 당시 '서부금융센터'를 약 3200억원에 사들였으며 이후 '센터포인트 웨스트'로 이름을 바꿨다.[김규리 기자 wizkim61@mkinternet.com]"
 # text = r"글로벌빅데이터연구소,?약?22만개?사이트?대상?9개?증권사?빅데이터?분석투자자?관심도?1위는?하나금융투자,?관심도?상승률?1위는?미래에셋대우  [파이낸셜뉴스]지난해 국내 주요 증권사에 대한 투자자 관심도를 조사한 결과 '하나금융투자'가 가장 높았던 것으로 나타났다. 같은 기간 관심도 상승률은 '미래에셋대우'가 가장 높았다.   5일 글로벌빅데이터연구소는 지난해 온라인 22만개 사이트를 대상으로 국내 9개 증권사에 대해 빅데이터를 분석한 결과, 이 같은 결과가 도출됐다고 밝혔다. 정보량의 경우 2019년과의 비교 분석도 실시했다.   연구소가 임의선정한 분석 대상 증권사는 '정보량 순'으로 △하나금융투자 △미래에셋대우 △NH투자증권 △키움증권 △삼성증권 △신한금융투자 △한국투자증권 △KB증권 △대신증권(대표 오익근) 등 이다.   분석 결과 온라인 게시물 수(총정보량)를 의미하는 '투자자 관심도'의 경우 2020년 '하나금융투자'는 총 30만2318건을 기록, 2019년 21만8533건에 비해 8만3785건 38.34% 늘어나며 1위를 차지했다.   이들 자료를 일일이 클릭한 결과 하나금융투자 정보량 중 '리포트'가 높은 비중을 차지했으며 투자자들은 이들 리포트를 블로그나 커뮤니티 등에 다시 게시하는 경우가 많았다.   정보량 2위는 지난해 총 29만1151건을 기록한 '미래에셋대우'였다. '미래에셋대우'는 지난 2019년 17만4672건에 비해서 11만6479건 66.68% 대폭 급증하며 증가량은 물론 증가율면에서 9개 주요 증권사중 가장 높았다.   2019년 26만3473건으로 가장 높은 관심도를 기록했던 'NH투자증권'은 지난해 2만3795건 9.03% 늘어 28만7268건을 보이는데 그치며 3위를 차지했다.   이어 '키움증권', '삼성증권', '신한금융투자', '한국투자증권', 'KB증권' 등이 20만~26만건 대를 기록하며 큰 차이를 보이지 않았으나 2019년 대비 증가량은 5.97%부터 50.72%까지 천차만별이었다.   관심도가 가장 낮은 '대신증권'은 지난해 총 19만7532건으로 2019년 13만8974건에 비해서는 5만8558건 42.14% 늘었다.   9개 증권사 중 가장 높은 투자자 호감도를 기록한 곳은 '하나금융투자'로 나타났다. 리포트 주목도가 높았던 '하나금융투자'는 긍정률에서 부정률을 뺀 값인 '순호감도'에서 41.94%를 기록, 1위를 차지했다.   정보량 상승률 1위였던 '미래에셋대우'가 28.94%로 순호감도에서 2위를 차지하며 '하나금융투자'와 함께 두 부문 모두 높은 지표를 보였다.   이어 '삼성증권' 25.78%, '한국투자증권' 25.36%, 'NH투자증권' 23.84%, '신한금융투자' 22.97%, '키움증권' 22.50%, 'KB증권' 21.41% 순이었다.   '대신증권'은 15.35%로 순호감도 역시 가장 낮았다"
 # text = r"우리나라의 1인가구가 매년 증가세를 보이는 가운데 지난해에는 600만을 넘어섰다. 가구 분포 역시 1인 가구는 30.2%를 차지해 2인 가구(27.8%), 3인 가구(20.7%), 4인 이상(21.2%)를 크게 앞지르며 대세를 이루고 있다. 특히 대전지역의 1인 가구 비율은 33.7%로 전년(32.6%) 대비 1.1% 증가했고 전국 1인 가구 비율보다는 3.5%가 더 높은 것으로 조사됐다. 향후 5년간 1인 가구 수가 매년 15만가구씩 증가할 것이라는 예측이 나오면서 유통업계에서는 1인 가구를 위한 소포장, 1인 메뉴 등을 출시하는데 열을 올리고 있다. 이러한 트렌드에 발맞춰 대전지역 롯데마트 3개 지점에서는 ‘한끼밥상’이라는 테마로 소포장 전문 코너를 만들어 운영 중이다. 농림축산식품부의 GAP(우수관리인증) 농산물을 990원부터 만나볼 수 있어 소비자로부터 좋은 반응을 보이고 있다. 실제로 대전지역 롯데마트 3개 지점(대덕점, 노은점, 서대전점)의 2020년 신선식품 中 소용량 상품군의 매출은 전년대비 12% 가까이 증가했다. 롯데마트 충청호남영업부문 배효권 부문장은 “1인 가구가 유통시장의 새로운 소비 주체로 떠오르면서 1~2인 가구를 겨냥한 소포장, 가정간편식 등과 같은 시장의 규모가 급성장할 것으로 예상된다”며 “롯데마트에서는 지역의 생산자와 손잡고 해당 상품군을 지속적으로 확대∙강화하는데 최선을 다하겠다”고 말했다."
+# text = r"정부가 이르면 26일께 3월부터 적용할 새 거리두기 조정안 단계를 발표할 예정이다. 오는 28일 현행 거리두기 단계(수도권 2단계, 비수도권 1.5단계) 종료 이후 사회적 거리두기가 재상향될지 관심이 모아진다.    손영래 중앙사고수습본부 사회전략반장은 23일 코로나19 백브리핑에서  (이번주) 금요일(26일) 또는 토요일(27일) 정도 생각 중인데 내일 정례브리핑 때 정확히 공지하겠다 고 밝혔다.    설 연휴 이후 600명대까지 치솟았던 일일 확진자수가 이틀 연속 300명대를 유지했지만 정부는 다시 증가할 가능성이 크다고 전망했다.    손 반장은  오늘까지는 주말 검사 감소량으로 인한 확진자 감소 현상이 나타났다고 본다 며  내일부터는 조금 증가할 것 같고, 26일까지 증가 추이가 어느 정도 갈지 봐야 한다 고 말했다.    앞서 정부는 지난 18일 다음 달부터 업종별 집합금지를 최소화하는 대신 개인 간 사적모임을 규제하는 자율과 책임에 기반을 골자로 하는 기본 방향을 내놨다.    이번 사회적 거리두기 개편안에는 현행 5단계(1→1.5→2→2.5→3단계)의 단점을 보완하는 대책도 담길 전망이다. 앞서 정부는 지난해 6월 3단계 체계의 거리두기를 적용하다가 같은해 11월 5단계로 개편한 바 있다. 0.5단계 차이로 세분화돼 있는 현행 체계는 단계별 대국민 행동 메시지가 분명하지 않아 위험성을 인지하기가 쉽지 않다는 지적이 제기돼 왔다.    식당이나 카페 등 다중이용시설에 대해서는 영업을 금지하는 집합금지는 최소화할 예정이다. 다만, 시설의 감염 취약 요인을 제거하기 위한 밀집도를 조정하기 위한 '인원제한'은 이어간다는 방침이다.    정세균 국무총리는 이날 중대본 회의에서  방역수칙 위반 업소에 대해서는 현재 시행 중인 '원스트라이크 아웃 제도'를 예외 없이 적용하고 곧 지급할 4차 재난지원금 지원 대상에서도 제외할 것 이라고 강조했다."
 s = Splitter(text)
 print(s.extnouns)
+# print(s.conndict)
+# print(s.trdict)
+# print(sorted(s.trdict, key=s.trdict.get, reverse=True)[:10]) # TR순 상위 10개 단어 출력
 
 # 새 방식
-# print(s.pairList)
-# print(s.conndict)
-# print(s.pmidict)
-# print(s.trdict)
-# print(sorted(s.trdict, key=s.trdict.get, reverse=True)[:10])
-# print(s.trpmidict)
-# print(s.detectConnection(s.pairList))
+# print(s.pairListNew)
+# print(s.pmidictnew)
+# print(s.trpmidictnew)
+# print(s.detectConnection(s.pairListNew))
+print([i for i in list(s.trpmidictnew.keys()) if s.trpmidictnew[i] >= 0.3])
 
 # 기존 방식
+# print(s.pairListOld)
+# print(s.pmidictold)
+# print(s.trpmidictold)
 # print(s.fList)
-print(s.allCW)
-# print(s.genCW(s.fList[0]))
+print([i for i in list(s.trpmidictold.keys()) if s.trpmidictold[i] >= 0.3])
